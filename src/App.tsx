d48296debb4d0914c18505dede7e177d036a8972
rashedrk <rashedrk001@gmail.com>
2024-01-11 22:00:10 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>My todo</h1>
+    <Button>My Button</Button>
+    </>
+  )
+}
+
+export default App
