cb76f27db10be7c596cd8147ba9bb030723c7ebf
rashedrk <rashedrk001@gmail.com>
2024-01-14 00:27:21 +0600

    add todo into redux store

--- src/components/todo/AddTodoModal.tsx
+++ src/components/todo/AddTodoModal.tsx
@@ -12,15 +12,22 @@ import {
 } from "../ui/dialog";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { useAppDispatch } from "@/redux/hooks";
+import { addTodo } from "@/redux/features/todoSlice";
 
 const AddTodoModal = () => {
   const [task, setTask] = useState("");
   const [description, setDescription] = useState("");
+  const dispatch = useAppDispatch();
 
   const onSubmit = (e: FormEvent) => {
     e.preventDefault();
     console.log({task, description});
-    
+    const taskDetails = {
+      title: task,
+      description
+    }
+    dispatch(addTodo(taskDetails));
   };
 
   return (
--- src/redux/features/todoSlice.ts
+++ src/redux/features/todoSlice.ts
@@ -1,6 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
+type TTodo = {
+    title: string,
+    description: string,
+    isCompleted?: boolean
+}
+
+type TInitialState = {
+    todo: TTodo[]
+}
+
+const initialState: TInitialState = {
     todo: []
 }
 
@@ -8,8 +18,12 @@ const todoSlice = createSlice({
     name: "todo",
     initialState,
     reducers: {
-
+        addTodo: (state, action: PayloadAction<TTodo>) => {
+            state.todo.push({ ...action.payload, isCompleted: false })
+        }
     }
 });
+
+export const {addTodo} = todoSlice.actions;
 
 export default todoSlice.reducer;
